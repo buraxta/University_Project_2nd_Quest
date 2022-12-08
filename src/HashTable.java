b228ef6219a10e01283eaa3c36b7545f72852820
Burak Güllüler <burakgulluler@gmail.com>
2022-12-08 19:46:49 +0300

    First Commit

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class HashTable {
+
+import java.util.LinkedList;
+
+public class HashTable {
+    private class Entry{
+        private String key;
+        private String value;
+
+        public Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    private LinkedList<Entry>[] entries = new LinkedList[10];
+    public void put(String key, String value){
+        var index = hash(key);
+        if (entries[index] == null)
+            entries[index] = new LinkedList<>();
+
+        var bucket = entries[index];
+        for (var entry : bucket){
+            if (entry.key == key){
+                entry.value = value;
+                return;
+            }
+        }
+
+        bucket.addLast(new Entry(key, value));
+    }
+
+    public String get(String key){
+        var index = hash(key);
+        var bucket = entries[index];
+        if (bucket != null){
+            for (var entry : bucket){
+                if (key.equals(entry.key))
+                    return entry.value;
+            }
+        }
+        return null;
+    }
+
+    private int hash(String key){
+        int hash = 0;
+        for (var ch : key.toCharArray()){
+            hash += ch;
+        }
+        return hash % entries.length;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+Create a dictionary of at least 30 words with Hash Table. Instead of using a linked list to resolve collisions,
+as in separate chaining, use a binary search tree. You’ll create a hash table that is an array of trees.
+(You can use Binary Heap of course. It’s up to you) (20 pt.)
+Note: Your dictionary must have at least four words which starts with same letter.
+And Words and meanings should read file from “my_words.txt”
+Word: computer / noun
+Meaning: an electronic machine that can store and arrange large amounts of information
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Create a hashTable
+        HashTable hashTable = new HashTable();
+
+        //Read values from file and add them into our created hashTable
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader("src/my_words.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] dic = line.split(":");
+                hashTable.put(dic[0],dic[1]);
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Now let's look at some word's meanings:
+        System.out.println(hashTable.get("campus"));
+        System.out.println(hashTable.get("carpenter"));
+        System.out.println(hashTable.get("challenge"));
+
     }
 }
